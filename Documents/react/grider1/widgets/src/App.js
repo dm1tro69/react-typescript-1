92789b1b60c55f6113cf8b6713cc6187f13911d5
dm1tro69 <dimolg22@gmail.com>
2021-03-05 20:56:07 +0200

    throtting api request

--- Documents/react/grider1/widgets/src/App.js
+++ Documents/react/grider1/widgets/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+    {title: "What is React",
+    content: 'React is a JavaScript framevork'
+    },
+    {title: "How do you React?",
+        content: 'You use React by creating components'
+    },
+    {title: "Why uses React?",
+        content: 'React is a favorite JS library'
+    },
+];
 
 function App() {
   return (
     <div >
-
+       {/*<Accordion items={items}/>*/}
+       <Search/>
     </div>
   );
 }
---
+++ Documents/react/grider1/widgets/src/components/Search.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+
+const Search = () => {
+    const [term, setTerm] = useState('')
+    const [result, setResult] = useState([])
+
+    useEffect(() => {
+       const search = async () => {
+          const {data} = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+
+            })
+           setResult(data.query.search)
+           console.log(result)
+
+       }
+     const timeoutId = setTimeout(()=> {
+           if (term){
+               search()
+
+           }
+       }, 500)
+     return ()=> {clearTimeout(timeoutId)}
+    }, [term])
+
+    const renderResults = result.map((res, i)=> {
+        return (
+            <div key={res.pageid} className="item">
+                <div className="right floated content">
+                    <a className={'ui button'} href={`https://en.wikipedia.org?curid=${res.pageid}`} target="_blank">Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {res.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: res.snippet}}></span>
+                </div>
+            </div>
+        )
+    })
+
+return (
+<div>
+    <div className="ui form">
+        <div className="field">
+            <label htmlFor="Search">Enter Search Term</label>
+            <input
+                name="Search"
+                type="text"
+                className="input"
+                value={term}
+                onChange={(e)=>setTerm(e.target.value)}
+            />
+        </div>
+    </div>
+    <div className="ui celled list">
+        {renderResults}
+
+    </div>
+</div>
+)
+}
+export default Search
