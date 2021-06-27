9f2ad57cdc0e356e8336995aa826cf7146d73e6a
dm1tro69 <dimolg22@gmail.com>
2021-06-27 19:08:48 +0300

    add markdown editor

---
+++ Documents/react3/grider/jbook/src/components/text-editor.tsx
@@ -0,0 +1,40 @@
+import {useState, useEffect, useRef} from "react";
+import MDEditor from '@uiw/react-md-editor'
+import './text-editor.css'
+
+const TextEditor: React.FC = () => {
+    const [editing, setEditing] = useState(false)
+    const [value, setValue] = useState('# Header')
+
+    const ref = useRef<HTMLDivElement | null>(null)
+
+    useEffect(()=> {
+        const listener = (e: MouseEvent) => {
+            if (ref.current && e.target && ref.current.contains(e.target as Node)){
+               return;
+            }
+            setEditing(false)
+        }
+        document.addEventListener('click', listener, {capture: true})
+        return ()=> {
+            document.removeEventListener('click', listener, {capture: true})
+        }
+    }, [])
+
+    if(editing){
+        return (
+            <div className={'text-editor'} ref={ref}>
+                <MDEditor value={value} onChange={(v)=> setValue(v || '')}/>
+            </div>
+        )
+    }
+
+    return (
+        <div className={'text-editor card'} onClick={()=> setEditing(true)}>
+            <div className={'card-content'}>
+        <MDEditor.Markdown source={value}/>
+            </div>
+        </div>
+    )
+}
+export default TextEditor
